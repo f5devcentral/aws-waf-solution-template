5c3ef620128a3f03c4b854dcd4c394ed2bcd9ac1
Shajiya Shaik <shajiya.parimala@gmail.com>
2021-11-05 18:46:26 +0530

    Adding the static verify page code

---
+++ scripts/pipeline/verify_cft_nap.py.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+from utils import *
+import sys
+
+nginx_default_page="Welcome to nginx"
+nginx_static_page="Hello World"
+nginx_dynamic_page="Arcadia Finance"
+stack_name=os.environ['STACK_NAME']
+page_type=sys.argv[1] 
+if page_type == "static":
+  page_type = nginx_static_page
+elif page_type == "dynamic":
+  page_type = nginx_dynamic_page
+else:
+  page_type = nginx_default_page
+#Verify functionality of NAP
+try:
+  vfy_status=vfy_cft_link(stack_name,page_type)
+  if vfy_status:
+    print("NGINX APP PROTECT ", sys.argv[1].upper() , "PAGE VERIFICATION IS COMPLETED SUCESSFULLY...")
+  else:
+    print("Error: NGINX APP PROTECT ", sys.argv[1].upper() , " PAGE VERIFICATION IS Failed...")
+except Exception as e:
+  print("Exception raised with error:",e)
